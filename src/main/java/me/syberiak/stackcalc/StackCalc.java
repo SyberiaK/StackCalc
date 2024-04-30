1fcc042d5c7089ecbb6efa818e84e9f0fd197350
SyberiaK <syberiakey@gmail.com>
2024-04-30 18:33:26 +0500

    1.20.4 support, some refactor

--- src/main/java/me/syberiak/stackcalc/StackCalc.java
+++ src/main/java/me/syberiak/stackcalc/StackCalc.java
@@ -1,5 +1,7 @@
 package me.syberiak.stackcalc;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.ArrayList;
 
 import org.slf4j.Logger;
@@ -12,10 +14,60 @@ public class StackCalc implements ClientModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("StackCalc");
 	public static final String SEPARATOR = " + ";
-    public static final int TAB_KEYCODE = 258;
+    public static final int DEFAULT_STACK_LIMIT = 64;
 
     @Override
     public void onInitializeClient() { LOGGER.info("Initialized successfully."); }
+    
+    public static Optional<String> processPrompt(String prompt) {
+        if (!prompt.contains("s") && !prompt.contains("sb") && !prompt.contains("dc")) {
+            return Optional.empty();
+        }
+
+        String call = prompt.substring(prompt.lastIndexOf(" ") + 1);
+        prompt = prompt.substring(0, prompt.lastIndexOf(" ") + 1);
+
+        String suffix;
+        if (call.contains("dc")) {
+            suffix = "dc";
+        } else if (call.contains("sb")) {
+            suffix = "sb";
+        } else if (call.contains("s")) {
+            suffix = "s";
+        } else return Optional.empty();
+
+        String limitStr = "";
+        if (call.contains("-")) {
+            String afterSuffixStr = call.substring(call.lastIndexOf("-") + 1);
+            if (isNumeric(afterSuffixStr) && afterSuffixStr.length() <= 9) {
+                limitStr = afterSuffixStr;
+            }
+        }
+
+        String countStr;
+        if (!limitStr.isEmpty()) {
+            countStr = call.substring(0, call.length() - (suffix + "-" + limitStr).length());
+        } else {
+            countStr = call.substring(0, call.length() - suffix.length());
+            limitStr = "%d".formatted(DEFAULT_STACK_LIMIT);
+        }
+
+        if (!isNumeric(countStr)) return Optional.empty();
+        if (countStr.length() > 9) return Optional.empty();
+
+        int count = Integer.parseInt(countStr);
+        int limit = Integer.parseInt(limitStr);
+
+        String result = switch (suffix) {
+            case "s": yield StackCalc.calculateStacks(count, limit);
+            case "sb": yield StackCalc.calculateShulkerBoxes(count, limit);
+            case "dc": yield StackCalc.calculateDoubleChests(count, limit);
+            default: yield "";
+        };
+
+        StackCalc.LOGGER.info("Got call: {}, result: {}", call, result);
+        return (prompt + result).describeConstable();
+    }
 
     public static String calculateShulkerBoxes(int count, int stackLimit) { 
         return calculateStorages(count, 27, stackLimit, "stackcalc.shulkerbox", "stackcalc.shulkerboxes");
@@ -33,12 +85,12 @@ public class StackCalc implements ClientModInitializer {
 
         int stacks = count / stackLimit;
         int items = count % stackLimit;
-        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> result = new ArrayList<>();
 
         if (stacks > 0) {
             String stackStringKey = (stacks == 1) ? "stackcalc.stack" : "stackcalc.stacks";
             String stackString = Text.translatable(stackStringKey, stacks).getString();
-            if (stackLimit != 64) stackString += String.format("(%d)", stackLimit);
+            if (stackLimit != DEFAULT_STACK_LIMIT) stackString += "(%d)".formatted(stackLimit);
             result.add(stackString);
         }
         if (items > 0) {
@@ -57,16 +109,20 @@ public class StackCalc implements ClientModInitializer {
 
         int storages = count / (stackLimit * slotsCount);
         int remainder = count % (stackLimit * slotsCount);
-        ArrayList<String> result = new ArrayList<String>();
+        List<String> result = new ArrayList<>();
 
         if (storages > 0) {
             String storageStringKey = (storages == 1) ? stringKey1 : stringKey2;
-            String storageString = Text.translatable(storageStringKey, storages).getString();
-            if (stackLimit != 64) storageString += String.format("(%d)", stackLimit);
+            String storageString = Text.translatable(storageStringKey, storages).toString();
+            if (stackLimit != DEFAULT_STACK_LIMIT) storageString += "(%d)".formatted(stackLimit);
             result.add(storageString);
         }
         if (remainder > 0) result.add(StackCalc.calculateStacks(remainder, stackLimit));
 
         return String.join(SEPARATOR, result);
+    }
+
+    private static boolean isNumeric(String str) {
+        return str.matches("^[0-9]+$");
     }
 }
--- src/main/java/me/syberiak/stackcalc/mixin/ChatScreenMixin.java
+++ src/main/java/me/syberiak/stackcalc/mixin/ChatScreenMixin.java
@@ -4,72 +4,37 @@ import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import me.syberiak.stackcalc.StackCalc;
 
+import java.util.NoSuchElementException;
+
 @Mixin(ChatScreen.class)
 public class ChatScreenMixin {
+
+    @Unique
+    private static final int TAB_KEYCODE = 258;
 
     @Shadow
     protected TextFieldWidget chatField;
 
     @Inject(at = @At("HEAD"), method = "keyPressed(III)Z")
     private void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<?> callbackInfo) {
-        if (keyCode == StackCalc.TAB_KEYCODE) {
-            String text = chatField.getText().trim();
-
-            if (text.contains("s") || text.contains("sb") || text.contains("dc")) {
-				String call = text.substring(text.lastIndexOf(" ") + 1);
-				text = text.substring(0, text.lastIndexOf(" ") + 1);
-
-                String suffix;
-                if (call.contains("dc")) { 
-                    suffix = "dc";
-                } else if (call.contains("sb")) { 
-                    suffix = "sb";
-                } else if (call.contains("s")) { 
-                    suffix = "s";
-                } else return;
-                
-                String limitStr = "";
-                if (call.contains("-")) {
-                    String afterSuffixStr = call.substring(call.lastIndexOf("-") + 1);
-                    if (isNumeric(afterSuffixStr) && afterSuffixStr.length() <= 9) {
-                        limitStr = afterSuffixStr;
-                    }
-                }
-                
-                String countStr;
-                if (limitStr != "") {
-                    countStr = call.substring(0, call.length() - (suffix + "-" + limitStr).length());
-                } else {
-                    countStr = call.substring(0, call.length() - suffix.length());
-                    limitStr = "64";
-                }
-
-                if (!isNumeric(countStr)) return;
-                if (countStr.length() > 9) return;
-
-                int count = Integer.parseInt(countStr);
-                int limit = Integer.parseInt(limitStr);
-
-                String result = switch (suffix) {
-                    case "s": yield StackCalc.calculateStacks(count, limit);
-                    case "sb": yield StackCalc.calculateShulkerBoxes(count, limit);
-                    case "dc": yield StackCalc.calculateDoubleChests(count, limit);
-                    default: yield "";
-                };
-                
-                chatField.setText(text + result);
-                StackCalc.LOGGER.info(String.format("Got call: %s, result: %s", call, result));
-            }
+        if (keyCode != TAB_KEYCODE) {
+            return;
         }
-    }
 
-    private static boolean isNumeric(String str) {
-        return str.matches("^[0-9]+$");
+        String text = chatField.getText().trim();
+
+        if (!text.contains("s") && !text.contains("sb") && !text.contains("dc")) return;
+
+        try {
+            String result = StackCalc.processPrompt(text).orElseThrow();
+            chatField.setText(result);
+        } catch (NoSuchElementException ignored) {}
     }
 }
